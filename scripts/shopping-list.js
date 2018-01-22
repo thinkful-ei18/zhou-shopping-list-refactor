2e95d5272f6a3cf749c2c2ad66b73e62bc14917b
Zhou <540376482yzb@gmail.com>
2018-01-22 09:59:39 -0800

    update shopping-list with store methods

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -50,16 +50,11 @@ const shoppingList = (function () {
 			event.preventDefault();
 			const newItemName = $('.js-shopping-list-entry').val();
 			$('.js-shopping-list-entry').val('');
-			addItemToShoppingList(newItemName);
+			store.addItem(newItemName)
+			// addItemToShoppingList(newItemName);
 			render();
 		});
 	}
-
-	function toggleCheckedForListItem(id) {
-		const foundItem = store.items.find(item => item.id === id);
-		foundItem.checked = !foundItem.checked;
-	}
-
 
 	function getItemIdFromElement(item) {
 		return $(item)
@@ -70,19 +65,10 @@ const shoppingList = (function () {
 	function handleItemCheckClicked() {
 		$('.js-shopping-list').on('click', '.js-item-toggle', event => {
 			const id = getItemIdFromElement(event.currentTarget);
-			toggleCheckedForListItem(id);
+			store.findAndToggle(id)
+			// toggleCheckedForListItem(id);
 			render();
 		});
-	}
-
-	function deleteListItem(id) {
-		const index = store.items.findIndex(item => item.id === id);
-		store.items.splice(index, 1);
-	}
-
-	function editListItemName(id, itemName) {
-		const item = store.items.find(item => item.id === id);
-		item.name = itemName;
 	}
 
 	function toggleCheckedItemsFilter() {
@@ -100,7 +86,7 @@ const shoppingList = (function () {
 			// get the index of the item in store.items
 			const id = getItemIdFromElement(event.currentTarget);
 			// delete the item
-			deleteListItem(id);
+			store.findAndDelete(id)
 			// render the updated shopping list
 			render();
 		});
@@ -111,7 +97,7 @@ const shoppingList = (function () {
 			event.preventDefault();
 			const id = getItemIdFromElement(event.currentTarget);
 			const itemName = $(event.currentTarget).find('.shopping-item').val();
-			editListItemName(id, itemName);
+			store.findAndUpdateName(id, itemName)
 			render();
 		});
 	}
